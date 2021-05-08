3c78146c89f4829853f216d666d74f12326fc3a8
Rosingsun <476348131@qq.com>
2021-05-08 22:41:00 +0800

    翻译页面ui+部分功能

--- ielp_Front/src/App.js
+++ ielp_Front/src/App.js
@@ -33,43 +33,7 @@ class App extends Component {
     return (
       <div className="App" >
         <header className="App-header">
-          {this.state.loaded == false ? <Login onLoaded={this.onLoaded} /> :
-            <Layout style={{ minHeight: '100vh' }}>
-              <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-                <div className="logo" />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                  {
-                    routesConfig.map((item, index) => {
-                      if (item.childrenPage == undefined && item.acehe==true) {
-                        return (
-                          <Menu.Item key={item.path} icon={<PieChartOutlined />} onClick={(e) => {
-                            this.setState({ panalName: item.title });
-                          }}>
-                            <Link to={item.path}>
-                              {item.title}
-                            </Link>
-                          </Menu.Item>
-                        )
-                      }
-                    })
-                  }
-                </Menu>
-              </Sider>
-              <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }} >
-                </Header>
-                <Content style={{ margin: '0 16px' }}>
-                  <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>数据管理</Breadcrumb.Item>
-                    <Breadcrumb.Item>{this.state.panalName}</Breadcrumb.Item>
-                  </Breadcrumb>
-                  <div style={{ height: '100%', width: '100%' }}>
-                    <PageConfig />
-                  </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>院科研 ©2021 Created by ZXY</Footer>
-              </Layout>
-            </Layout>}
+          {this.state.loaded == false ? <Login onLoaded={this.onLoaded} /> : <PageConfig />}
         </header>
       </div>
     );
--- ielp_Front/src/views/translate/index.jsx
+++ ielp_Front/src/views/translate/index.jsx
@@ -1,41 +1,51 @@
 import React, { Component } from 'react';
-import { Input, Button,message } from 'antd';
+import { Input, Button, message, Select } from 'antd';
 import s from "./style.module.scss";
 import * as common from '@actions/common';
-import { APPID, pass } from "@constants/apiConstants.js"; 
-import {MD5} from "../../utils/MD5"; 
+import { APPID, pass } from "@constants/apiConstants.js";
+import { MD5 } from "../../utils/MD5";
+const { TextArea } = Input;
+const { Option } = Select;
 class translate extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputWord: '',
+      inputWord: '',//用户输入的文字
+      dst: "",//翻译出来的目标文字
+      from: "zh",
+      to : 'en',
+      list: [
+        {
+          name: "中文",
+          value: "zh",
+        }, {
+          name: "英语",
+          value: "en"
+        }
+      ]
     }
   }
   getWord = (e) => {
-    console.log(e);
     // 获取前端输入的数据
     this.setState({ inputWord: e });
   }
   transport = (e) => {
-    console.log(APPID,pass)
     var salt = (new Date).getTime();
     var query = this.state.inputWord;
-    var from = 'zh';
-    var to = 'en';
     var str1 = APPID + query + salt + pass;
     var sign = MD5(str1);
-    
+
     common.transport({
       q: query,
       appid: APPID,
       salt: salt,
-      from: from,
-      to: to,
+      from: this.state.from,
+      to: this.state.to,
       sign: sign,
     }).then(res => {
-      console.log(res);
-      if(res.status==200){
-        message.success(res.data.trans_result[0].dst)
+      if (res.status == 200) {
+        message.success("翻译成功")
+        this.setState({ dst: res.data.trans_result[0].dst })
       }
     }).catch(err => {
       console.log(err);
@@ -44,9 +54,51 @@ class translate extends Component {
   render() {
     return (
       <div className={s.body}>
-        <div style={{ width: '30vw' }}>
-          <Input placeholder="你想翻译的句子" onChange={(e) => { this.getWord(e.target.value) }} />
-          <Button type="primary" onClick={() => { this.transport() }} style={{ marginTop: '20px', width: '100%', height: '60px' }}>翻译 </Button>
+        <div className={s.translateBox}>
+          <div className={s.left}>
+            <div className={s.leftTop}>
+              <div className={s.topChoic}>
+                <Select placeholder="请选择输入的语言" defaultValue="中文" style={{ width: '40%' }} 
+                onChange={(e)=>{
+                  this.setState({from:e})
+                }}>
+                  {
+                    this.state.list.map((item, index) => {
+                      return (
+                        <Option key={index} value={item.value}>{item.name}</Option>
+                      )
+                    })
+                  }
+
+                </Select>
+                <Select placeholder="请选择目标语言" defaultValue="英语" style={{ width: '40%' }}
+                onChange={(e)=>{
+                  this.setState({to:e})
+                }}>
+                  {
+                    this.state.list.map((item, index) => {
+                      return (
+                        <Option key={index} value={item.value}>{item.name}</Option>
+                      )
+                    })
+                  }
+                </Select>
+                <Button type="primary" onClick={() => { this.transport() }}>翻译 </Button>
+              </div>
+              <div className={s.Input}>
+                <TextArea autoSize={{ minRows: 5, maxRows: 6 }} placeholder="你想翻译的句子" style={{ height: '120px', marginTop: "20px", width: "95%" }} onChange={(e) => { this.getWord(e.target.value) }} />
+              </div>
+            </div>
+            <div className={s.leftBottom}>
+
+            </div>
+          </div>
+          <div className={s.right}>
+            <div className={s.rightTop}>
+              <div className={s.content}>{this.state.dst}</div>
+            </div>
+            <div className={s.rightBtm}></div>
+          </div>
         </div>
       </div>
     )
