7518e3ce11a9008d54869cb50bc345ad89d6125a
Git1 <2482394937@qq.com>
2021-05-15 23:44:53 +0800

    弹框

--- ielp_Front/src/views/Home/index.jsx
+++ ielp_Front/src/views/Home/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import a from "./style.module.scss";
 export default class Home extends Component {
   constructor(props) {
     super(props);
@@ -7,7 +8,19 @@ export default class Home extends Component {
   }
   render() {
     return (
-      <div>213123123</div>
+      <div className={a.one}>
+        <div className={a.three}>
+          <img src={require('./abc.png').default} width='30%' ></img>
+        </div>
+          <div className={a.two}>
+            <div>
+              <a className={a.text1}>今日未打卡</a>
+            </div>
+            <button className={a.btn}>
+              <a className={a.text1}>今日未打卡</a>
+            </button>
+        </div>
+      </div>
     );
   }
 }
