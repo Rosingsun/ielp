feeae26fa0f7450c7a1f2befc02745ea151decea
Rosingsun <476348131@qq.com>
2021-05-16 21:08:58 +0800

    修改样式

--- ielp_Front/src/index.js
+++ ielp_Front/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { createHashHistory } from 'history'; // hash路由
-import App from './views/Home/Moretext';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
