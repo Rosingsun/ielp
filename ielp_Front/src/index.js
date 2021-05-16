833204c2fbeb4c9d0c7573ade8b35fb8b0223774
Git1 <2482394937@qq.com>
2021-05-16 23:19:30 +0800

    打卡按钮

--- ielp_Front/src/index.js
+++ ielp_Front/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { createHashHistory } from 'history'; // hash路由
-import App from './App';
+import App from '../src/views/Home/index';
 
 ReactDOM.render(
   <React.StrictMode>
--- ielp_Front/src/views/Home/index.jsx
+++ ielp_Front/src/views/Home/index.jsx
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
 import a from "./style.module.scss";
+import { Button ,Divider} from 'antd';
+
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // currentTime:formateData(Data.now()),
     };
+    // getTime=()=>{
+    //   setInterval(()=>{
+    //     const currentTime=formateData(Date.now())
+    //     this.setState({currentTime})
+    //   },1000)
+    // }
+    // componentDidMount() {
+    //   this.getTime()
   }
   render() {
+    // const {currentTime}=this.state
     return (
       <div className={a.one}>
-        <div className={a.three}>
-          <img src={require('./abc.png').default} width='30%' ></img>
-        </div>
+        
+        <div className={a.left}>
+          <div className={a.time}>
+          </div>
           <div className={a.two}>
-            <div>
+            <Button type="primary" className={a.btn1}>
               <a className={a.text1}>今日未打卡</a>
-            </div>
-            <button className={a.btn}>
-              <a className={a.text1}>今日未打卡</a>
-            </button>
+            </Button>
+            <Button type="primary" className={a.btn} onClick={() => {}}>
+              <a className={a.text1}>马上去打卡</a>
+            </Button>
+        </div> 
+        </div>
+        <div className={a.right}>
+          <img src={require('./abc.png').default} width='170%' height='75%' margin-right='50px' ></img>
         </div>
       </div>
     );
