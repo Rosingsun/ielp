babdfb1908090ab86b98e6339989acdc5c3e4d00
幕冬 <mudongheng@foxmail.com>
2021-11-24 10:04:50 +0800

    新增：获取用户信息

--- ielp_Back/src/main/java/com/mudongheng/ielp/controller/UserInfoController.java
+++ ielp_Back/src/main/java/com/mudongheng/ielp/controller/UserInfoController.java
@@ -1,7 +1,12 @@
 package com.mudongheng.ielp.controller;
 
+import com.mudongheng.ielp.exception.UserException;
+import com.mudongheng.ielp.model.entity.User;
+import com.mudongheng.ielp.service.UserService;
+import com.mudongheng.ielp.util.UserUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserInfoController {
 
-    @PostMapping("/hello")
-    public String hello() throws Exception {
-        throw new Exception("hi");
+    private final UserService userService;
+
+    public UserInfoController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/get-user-info-by-id")
+    public User getUserInfoById(@RequestBody Integer id) throws Exception {
+        User user = userService.getById(id);
+        if (user == null) {
+            throw new UserException("用户不存在");
+        }
+        return UserUtil.removeUserPassword(userService.getById(id));
     }
 
 }
